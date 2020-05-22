b1c7807b1752c397ee34e41496d632daeddc8cda
Erick Rincones <erick.rincones@ciens.ucv.ve>
2020-05-22 03:42:55 -0400

    Auto select cow on click

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,32 @@
 import React, { useState } from "react";
 import { Controls } from "../components/controls";
 import { Terminal } from "../components/terminal";
+import { Position } from "../components/shared";
+
+const mouse: Position = { x: NaN, y: NaN };
+
+const handleMouseDown = (e: React.MouseEvent): void => {
+  mouse.x = e.clientX;
+  mouse.y = e.clientY;
+};
+
+const handleMouseUp = (e: React.MouseEvent<HTMLPreElement>): void => {
+  if ((mouse.x !== e.clientX) || (mouse.y !== e.clientY)) {
+    return;
+  }
+
+  if (window.getSelection) {
+    e.preventDefault();
+    e.stopPropagation();
+
+    const selection = window.getSelection();
+    const range = document.createRange();
+
+    range.selectNodeContents(e.currentTarget);
+    selection?.removeAllRanges();
+    selection?.addRange(range);
+  }
+}
 
 const Home = (): JSX.Element => {
   const [ cow, setCow ] = useState(``);
@@ -11,7 +37,7 @@ const Home = (): JSX.Element => {
     <div className="scrolling-touch grid grid-flow-row md:grid-flow-col md:grid-cols-12 md:h-screen">
       <Controls className="md:col-start-8 md:col-span-5 md:h-full" onChange={handleCow} />
       <Terminal className="cursor-text md:col-start-1 md:col-span-7">
-        <pre>{cow}</pre>
+        <pre onMouseDown={handleMouseDown} onMouseUp={handleMouseUp}>{cow}</pre>
       </Terminal>
     </div>
   );
