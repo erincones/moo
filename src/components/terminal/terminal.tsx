3f1382d7cd9e1d6da733c4eb79c13d1b7dfbe1cb
Erick Rincones <erick.rincones@ciens.ucv.ve>
2020-05-22 01:09:55 -0400

    Add the history command in terminal
    Sort commands by alphabetic order

--- src/components/terminal/terminal.tsx
+++ src/components/terminal/terminal.tsx
@@ -7,6 +7,13 @@ interface Props extends ClassName {
   readonly children?: React.ReactNode;
 }
 
+interface History {
+  command: string;
+  root: boolean;
+}
+
+const history: History[] = [];
+
 export const Terminal = ({ className, children }: Props): JSX.Element => {
   const input = useRef<HTMLSpanElement>(null);
   const [ root, setRoot ] = useState(false);
@@ -29,6 +36,10 @@ export const Terminal = ({ className, children }: Props): JSX.Element => {
     }
 
     const text = input.current.innerHTML.replace(/(?:\n|<br>)/g, ``);
+    if (text.trim()) {
+      history.push({ command: text, root });
+    }
+
     if (/^\s*(?:sudo\s+)*clear(?:\s+.*)?$/.test(text)) {
       setOutput([]);
       input.current.innerHTML = `<br>`;
@@ -42,23 +53,10 @@ export const Terminal = ({ className, children }: Props): JSX.Element => {
     ];
 
     let match: RegExpMatchArray | null = null;
-
     if ((match = text.match(/^\s*(?:sudo\s+)*echo(\s+.+)?$/))) {
       if (match[1]) {
         nodes.push(<Output key={key + 2}>{match[1].trim()}</Output>);
       }
-    }
-    else if (/^\s*(?:sudo\s+)*ls(?:\s+.*)?$/.test(text)) {
-      nodes.push(
-        <Output key={key + 2}>
-          <span className="font-bold text-blue-light">.</span>
-          &nbsp;&nbsp;
-          <span className="font-bold text-blue-light">..</span>
-        </Output>
-      );
-    }
-    else if (/^\s*(sudo\s+)+su(?:\s+.*)?$/.test(text)) {
-      setRoot(true);
     }
     else if (/^(?:sudo\s+)*exit(?:\s+.*)?$/.test(text)) {
       setRoot(false);
@@ -93,6 +91,32 @@ export const Terminal = ({ className, children }: Props): JSX.Element => {
         </Output>
       );
     }
+    else if ((match = text.match(/^\s*(?:sudo\s+)*history(\s+-c)?(?:\s+.+)?$/))) {
+      if (match[1]) {
+        history.splice(0, history.length, ...history.filter(({ root: su }) => (
+          su !== root
+        )));
+      }
+      else {
+        history.filter(({ root: su }) => (
+          su === root
+        )).forEach(({ command }, i) => {
+          nodes.push(<Output key={key + i + 2}>{command}</Output>);
+        });
+      }
+    }
+    else if (/^\s*(?:sudo\s+)*ls(?:\s+.*)?$/.test(text)) {
+      nodes.push(
+        <Output key={key + 2}>
+          <span className="font-bold text-blue-light">.</span>
+          &nbsp;&nbsp;
+          <span className="font-bold text-blue-light">..</span>
+        </Output>
+      );
+    }
+    else if (/^\s*(sudo\s+)+su(?:\s+.*)?$/.test(text)) {
+      setRoot(true);
+    }
     else if ((match = text.match(/^\s*(?:sudo\s+)*((?!sudo\b)\S+)(?:\s+.*)?$/))) {
       nodes.push(
         <Output key={key + 2}>
@@ -117,10 +141,10 @@ export const Terminal = ({ className, children }: Props): JSX.Element => {
   return (
     <div className={mergeClasses(`flex flex-col overflow-x-auto max-w-full px-px`, className)}>
       {children}
-      <pre className="block flex-grow w-full" onPaste={handlePaste} onClick={handleFocus}>
+      <pre className="block flex-grow w-full" onPaste={handlePaste} onClick={handleFocus} onFocus={handleFocus}>
         {output}
         <Prompt root={root} dir="moo" />
-        <span ref={input} autoCapitalize="off" spellCheck={false} contentEditable suppressContentEditableWarning className="whitespace-pre outline-none" onFocus={handleFocus} onInput={handleInput}>
+        <span ref={input} autoCapitalize="off" spellCheck={false} contentEditable suppressContentEditableWarning className="whitespace-pre outline-none" onInput={handleInput}>
           <br />
         </span>
       </pre>
