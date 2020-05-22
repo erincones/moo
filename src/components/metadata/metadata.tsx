dd28cebd8c3b760c08a7a4af557d66474cfc9fb0
Erick Rincones <erick.rincones@ciens.ucv.ve>
2020-05-22 08:47:05 -0400

    Improve the SEO metadata

--- src/components/metadata/metadata.tsx
+++ src/components/metadata/metadata.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import { Helmet } from "react-helmet";
 
+interface Resolution {
+  w: string;
+  h: string;
+}
+
 const title = `moo!`;
 const description = `Another web version of cowsay`;
 const url = `https://erincones.github.io/moo`;
 const image = `${url}/cover.png`;
+const resolution: Resolution = { w: `1920`, h: `1080` };
 
 export const Metadata = (): JSX.Element => (
   <Helmet defer={false}>
@@ -21,10 +27,15 @@ export const Metadata = (): JSX.Element => (
     <meta property="og:description" content={description} />
     <meta property="og:url" content={url} />
     <meta property="og:image" content={image} />
+    <meta property="og:image:width" content={resolution.w} />
+    <meta property="og:image:height" content={resolution.h} />
 
-    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:card" content="summary_large_image" />
     <meta name="twitter:title" content={title} />
     <meta name="twitter:description" content={description} />
+    <meta name="twitter:creator" content="@erickrincones" />
     <meta name="twitter:image" content={image} />
+
+    <meta name="google-site-verification" content="RScugNg5R9w-kAFDk8kWA0WqSmY9_UXU4fe1Y9KfjJs" />
   </Helmet>
 );
