bcf979f3671412758e57cc4c2c8e9131e1a52483
Erick Rincones <erick.rincones@ciens.ucv.ve>
2020-05-23 23:44:11 -0400

    Updates

--- gatsby-ssr.js
+++
@@ -1,7 +0,0 @@
-/**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/ssr-apis/
- */
-
-import "./src/styles/scss/index.scss";
--- src/components/cowsay/index.tsx
+++
@@ -1,7 +0,0 @@
-export type { Cow } from "./cows";
-export type { Action } from "./cowsay";
-export type { Mode } from "./modes";
-
-export { cows } from "./cows";
-export { modes, getMode, getFace } from "./modes";
-export { cowsay, cowthink } from "./cowsay";
